b9a00b5b4adb1b0206e9c22500a10b878fbf6d3b
Canmor Lam <canmor.lam@gmail.com>
2022-11-03 18:11:25 +0800

    FetchContent for asio

--- cmake/asio.cmake
+++ cmake/asio.cmake
@@ -6,29 +6,18 @@ set(ASIO_GIT_TAG "asio-1-21-0" CACHE STRING "asio Git Repository tag")
 if (NOT TARGET asio::asio)
     find_package(asio QUIET)
     if (NOT asio_FOUND)
-        include(ExternalProject)
+        include(FetchContent)
         if (ASIO_URL STREQUAL "")
-            ExternalProject_Add(asio_external
-                    GIT_REPOSITORY ${ASIO_GIT_REPOSITORY}
-                    GIT_TAG ${ASIO_GIT_TAG}
-                    CONFIGURE_COMMAND ""
-                    BUILD_COMMAND ""
-                    INSTALL_COMMAND "")
+            FetchContent_Declare(asio GIT_REPOSITORY ${ASIO_GIT_REPOSITORY} GIT_TAG ${ASIO_GIT_TAG})
         else ()
-            ExternalProject_Add(asio_external
-                    URL ${ASIO_URL}
-                    URL_HASH ${ASIO_URL_HASH}
-                    CONFIGURE_COMMAND ""
-                    BUILD_COMMAND ""
-                    INSTALL_COMMAND "")
+            FetchContent_Declare(asio URL ${ASIO_URL} URL_HASH ${ASIO_URL_HASH})
         endif ()
-        ExternalProject_Get_Property(asio_external SOURCE_DIR)
+        FetchContent_MakeAvailable(asio)
         add_library(asio INTERFACE)
-        target_include_directories(asio INTERFACE ${SOURCE_DIR}/asio/include)
+        target_include_directories(asio INTERFACE ${asio_SOURCE_DIR}/asio/include)
         if (WIN32)
             target_link_libraries(asio INTERFACE Ws2_32)
         endif ()
-        add_dependencies(asio asio_external)
         add_library(asio::asio ALIAS asio)
     endif ()
 endif ()
